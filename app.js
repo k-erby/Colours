e56fff29d14f38dddf6886f4f688eb1208195bd5
Kaitlin <cmykaitlin@gmail.com>
2016-10-27 19:54:54 -0700

    Added jitter to the end slide.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ window.onload = function() {
 };
 
 
-// for the jitter effect animation
+// for the jitter effect animation for the start
 $(function(){
   var $jittery = $('.jiterry'),
       aText    = $jittery.text().split(""),
@@ -26,6 +26,23 @@ $(function(){
   for(var i = 0; i < aText.length; i++){
     letters += '<span>'+aText[i]+'</span>';
   }
+
+  $jittery.empty().append(letters);
+
+  $.each($('span', $jittery), function(i){
+    $(this).css('animation-delay', '-'+i+'70ms');
+  });
+});
+
+$(function(){
+  var $jittery = $('.jiterry2'),
+      aText    = $jittery.text().split(""),
+      letters = '';
+
+  for(var i = 0; i < aText.length; i++){
+    letters += '<span>'+aText[i]+'</span>';
+  }
+  console.log("hi");
 
   $jittery.empty().append(letters);
 
