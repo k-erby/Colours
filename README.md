8f8b1fd843e7cc7f178c205caa266a6400e8160f
Kaitlin <cmykaitlin@gmail.com>
2019-05-12 11:10:58 -0700

    Demolish unneeded/updated parts of the repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Colours
+
+Little baby project I did as a gift for my mom. 
+
+I probably _should_ go through this and fix everything, but that seems like
+too much effort for too little reward.
--- jitter.js
+++ jitter.js
@@ -1,32 +1,12 @@
-// loader
-window.onload = function() {
-   setTimeout(function(){
-     var fullscreen = document.getElementById('loader');
-     fullscreen.style.display = "none";
-   }, 1700);
-
-   setTimeout(function(){
-     var title = document.getElementById('title-page');
-     title.style.display = "block";
-   }, 1700);
-
-   setTimeout(function(){
-     var audioPlayer = document.getElementById('audio');
-     audioPlayer.play();
-   }, 1800);
-};
-
-
 // for the jitter effect animation for the start
 $(function(){
-  var $jittery = $('.jiterry'),
+  var $jittery = $('.jittery'),
       aText    = $jittery.text().split(""),
       letters = '';
 
   for(var i = 0; i < aText.length; i++){
     letters += '<span>'+aText[i]+'</span>';
   }
-
   $jittery.empty().append(letters);
 
   $.each($('span', $jittery), function(i){
@@ -35,15 +15,13 @@ $(function(){
 });
 
 $(function(){
-  var $jittery = $('.jiterry2'),
+  var $jittery = $('.jittery2'),
       aText    = $jittery.text().split(""),
       letters = '';
 
   for(var i = 0; i < aText.length; i++){
     letters += '<span>'+aText[i]+'</span>';
   }
-  console.log("hi");
-
   $jittery.empty().append(letters);
 
   $.each($('span', $jittery), function(i){
